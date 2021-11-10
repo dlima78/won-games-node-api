9293f70a41ca86bae529235870ebf9aa481a41c7
Eduardo Lima <dlima78@hotmail.com>
2021-11-10 14:53:43 -0300

    chore: config paths

--- jest.config.ts
+++ jest.config.ts
@@ -6,5 +6,9 @@ export default {
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1'
   }
 }
