2baf285924304d0589cb09798bb1910989789517
Eduardo Lima <dlima78@hotmail.com>
2021-11-11 14:30:47 -0300

    chore: add jest configs and scritps

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
-export default {
+module.exports = {
   roots: ['<rootDir>/tests'],
-  collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
