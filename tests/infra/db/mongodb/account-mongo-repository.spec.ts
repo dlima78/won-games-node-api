31cc96969dfca048493975edb60b10b27d4f6c47
Eduardo Lima <dlima78@hotmail.com>
2021-11-15 08:58:26 -0300

    refactor: clear database between tests

--- tests/infra/db/mongodb/account-mongo-repository.spec.ts
+++ tests/infra/db/mongodb/account-mongo-repository.spec.ts
@@ -12,6 +12,12 @@ describe('Account Mongo Repository', () => {
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('Should return an account on success', async () => {
     const sut = makeSut()
     const account = await sut.add({
