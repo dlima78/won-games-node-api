9c309665568128f3de53310f763e2697fd0d2939
Eduardo Lima <dlima78@hotmail.com>
2021-11-15 10:36:15 -0300

    feat: ensure cors is enabled

---
+++ tests/main/middleware/cors.test.ts
@@ -0,0 +1,15 @@
+import request from 'supertest'
+import app from '@/main/config/app'
+
+describe('CORS Middleware', () => {
+  test('Should enable CORS', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send()
+    })
+    await request(app)
+      .get('/test_cors')
+      .expect('Access-Control-Allow-Origin', '*')
+      .expect('Access-Control-Allow-Methods', '*')
+      .expect('Access-Control-Allow-Headers', '*')
+  })
+})
