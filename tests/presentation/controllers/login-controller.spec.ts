3f8ab37df13b010963321295d2c84ea7cc640bfc
Eduardo Lima <dlima78@hotmail.com>
2021-11-17 13:57:40 -0300

    test: ensure LoginController returns 200 if Authentication success

--- tests/presentation/controllers/login-controller.spec.ts
+++ tests/presentation/controllers/login-controller.spec.ts
@@ -9,6 +9,7 @@ import {
 } from '@/presentation/errors'
 import {
   badRequest,
+  ok,
   serverError,
   unauthorized
 } from '@/presentation/helpers/http-helper'
@@ -123,5 +124,11 @@ describe('Login Controller', () => {
       .mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(serverError(new ServerError(null)))
+  })
+
+  test('Should return 200 if Authentication success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(ok({ accessToken: 'any_token' }))
   })
 })
