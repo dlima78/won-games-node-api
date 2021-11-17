9204ebe3f1c37bb2d4231dc7420d6d24e69251d9
Eduardo Lima <dlima78@hotmail.com>
2021-11-17 10:32:46 -0300

    test: ensure LoginController calls EmailValidator with correct email

--- tests/presentation/controllers/login-controller.spec.ts
+++ tests/presentation/controllers/login-controller.spec.ts
@@ -1,15 +1,29 @@
 import { LoginController } from '@/presentation/controllers'
 import { MissingParamError } from '@/presentation/errors'
 import { badRequest } from '@/presentation/helpers/http-helper'
+import { EmailValidator } from '@/presentation/protocols'
+import faker from 'faker'
 
-interface SutTypes {
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorSpy implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorSpy()
+}
+
+type SutTypes = {
   sut: LoginController
+  emailValidatorSpy: EmailValidator
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new LoginController()
+  const emailValidatorSpy = makeEmailValidator()
+  const sut = new LoginController(emailValidatorSpy)
   return {
-    sut
+    sut,
+    emailValidatorSpy
   }
 }
 
@@ -18,7 +32,7 @@ describe('Login Controller', () => {
     const { sut } = makeSut()
     const httpRequest = {
       body: {
-        password: 'any_password'
+        password: faker.internet.password()
       }
     }
     const httpResponse = await sut.handle(httpRequest)
@@ -29,10 +43,23 @@ describe('Login Controller', () => {
     const { sut } = makeSut()
     const httpRequest = {
       body: {
-        email: 'any_email@mail.com'
+        email: faker.internet.email()
       }
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
+  })
+
+  test('Should call EmailValidator with correct email', async () => {
+    const { sut, emailValidatorSpy } = makeSut()
+    const isValidSpy = jest.spyOn(emailValidatorSpy, 'isValid')
+    const httpRequest = {
+      body: {
+        email: faker.internet.email(),
+        password: faker.internet.password()
+      }
+    }
+    await sut.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenCalledWith(httpRequest.body.email)
   })
 })
