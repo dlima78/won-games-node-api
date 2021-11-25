b57c6c6bc629f87db174f1260b5329952af26113
Eduardo Lima <dlima78@hotmail.com>
2021-11-25 11:29:36 -0300

    refactor: separate factories in folders

--- src/main/factories/controllers/login-controller-factory.ts
+++ src/main/factories/controllers/login-controller-factory.ts
@@ -1,19 +1,10 @@
-import { DbAuthentication } from '@/data/usecases'
-import { BcryptAdapter, JwtAdapter } from '@/infra/cryptography'
-import { AccountMongoRepository, LogMongoRepository } from '@/infra/db/mongodb'
-import { LogControllerDecorator } from '@/main/decorators/log-controller-decorator'
 import { LoginController } from '@/presentation/controllers'
 import { Controller } from '@/presentation/protocols'
 import { makeLoginValidation } from '@/main/factories/controllers'
-import env from '@/main/config/env'
+import { makeDbAuthentication } from '@/main/factories/usecases'
+import { makeLogControllerDecorator } from '@/main/factories/decorator'
 
 export const makeLoginController = (): Controller => {
-  const salt = 12
-  const bcryptAdapter = new BcryptAdapter(salt)
-  const jwtAdapter = new JwtAdapter(env.jwtSecret)
-  const accountMongoRepository = new AccountMongoRepository()
-  const dbAuthentication = new DbAuthentication(accountMongoRepository, bcryptAdapter, jwtAdapter, accountMongoRepository)
-  const loginController = new LoginController(dbAuthentication, makeLoginValidation())
-  const logMongoRepository = new LogMongoRepository()
-  return new LogControllerDecorator(loginController, logMongoRepository)
+  const loginController = new LoginController(makeDbAuthentication(), makeLoginValidation())
+  return makeLogControllerDecorator(loginController)
 }
--- src/main/factories/controllers/signup-controller-factory.ts
+++ src/main/factories/controllers/signup-controller-factory.ts
@@ -1,17 +1,10 @@
-import { DbAddAccount } from '@/data/usecases'
-import { BcryptAdapter } from '@/infra/cryptography'
-import { AccountMongoRepository, LogMongoRepository } from '@/infra/db/mongodb'
-import { LogControllerDecorator } from '@/main/decorators/log-controller-decorator'
 import { SignUpController } from '@/presentation/controllers'
 import { Controller } from '@/presentation/protocols'
-import { makeSignUpValidation } from '.'
+import { makeSignUpValidation } from '@/main/factories/controllers'
+import { makeDbAddAccount, makeDbAuthentication } from '@/main/factories/usecases'
+import { makeLogControllerDecorator } from '../decorator'
 
 export const makeSignUpController = (): Controller => {
-  const salt = 12
-  const bcryptAdapter = new BcryptAdapter(salt)
-  const accountMongoRespository = new AccountMongoRepository()
-  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRespository)
-  const signupController = new SignUpController(dbAddAccount, makeSignUpValidation())
-  const logMongoRepository = new LogMongoRepository()
-  return new LogControllerDecorator(signupController, logMongoRepository)
+  const signupController = new SignUpController(makeDbAddAccount(), makeSignUpValidation(), makeDbAuthentication())
+  return makeLogControllerDecorator(signupController)
 }
