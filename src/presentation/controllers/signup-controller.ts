d3b24d3bc339f6dd4039da661814d797d77d15db
Eduardo Lima <dlima78@hotmail.com>
2021-11-25 09:51:43 -0300

    feat: ensure SignUpController returns an accessToken on success

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -22,13 +22,13 @@ export class SignUpController implements Controller {
         return badRequest(error)
       }
       const { password, email, name } = httpRequest.body
-      const account = await this.addAccount.add({
+      await this.addAccount.add({
         name,
         email,
         password
       })
-      await this.authentication.auth({ email, password })
-      return ok(account)
+      const accessToken = await this.authentication.auth({ email, password })
+      return ok({ accessToken })
     } catch (error) {
       return serverError(error)
     }
