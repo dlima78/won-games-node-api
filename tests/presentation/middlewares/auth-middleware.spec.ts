076889b87b6d40424d4bb1ed36a1f6b4705f1f17
Eduardo Lima <dlima78@hotmail.com>
2021-11-29 15:43:01 -0300

    test: ensure AuthMiddleware calls LoadAccountByToken with correct token

--- tests/presentation/middlewares/auth-middleware.spec.ts
+++ tests/presentation/middlewares/auth-middleware.spec.ts
@@ -1,15 +1,43 @@
+import { AccountModel } from '@/domain/models/account'
+import { LoadAccountByToken } from '@/domain/usecases/load-account-by-token'
 import { AccessDeniedError } from '@/presentation/errors'
 import { forbiden } from '@/presentation/helpers/http-helper'
 import { AuthMiddleware } from '@/presentation/middlewares'
+import { HttpRequest } from '@/presentation/protocols'
+
+const makeFakeRequest = (): HttpRequest => ({
+  headers: {
+    'x-access-token': 'any-token'
+  }
+})
+
+const mockAccount = (): AccountModel => ({
+  id: 'any-id',
+  name: 'any-name',
+  email: 'email@mail.com',
+  password: 'any-password'
+})
+
+const makeLoadAccountByToken = (): LoadAccountByToken => {
+  class LoadAccountByTokenSpy implements LoadAccountByToken {
+    async load (accessToken: string, role?: string): Promise<AccountModel> {
+      return await Promise.resolve(mockAccount())
+    }
+  }
+  return new LoadAccountByTokenSpy()
+}
 
 type SutTypes = {
   sut: AuthMiddleware
+  loadAccountByTokenSpy: LoadAccountByToken
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new AuthMiddleware()
+  const loadAccountByTokenSpy = makeLoadAccountByToken()
+  const sut = new AuthMiddleware(loadAccountByTokenSpy)
   return {
-    sut
+    sut,
+    loadAccountByTokenSpy
   }
 }
 
@@ -18,5 +46,12 @@ describe('Auth Middleware', () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(forbiden(new AccessDeniedError()))
+  })
+
+  test('Should call LoadAccountByToken with correct accessToken', async () => {
+    const { sut, loadAccountByTokenSpy } = makeSut()
+    const loadSpy = jest.spyOn(loadAccountByTokenSpy, 'load')
+    await sut.handle(makeFakeRequest())
+    expect(loadSpy).toHaveBeenCalledWith('any-token')
   })
 })
