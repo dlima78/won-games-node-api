6d41fc6ddf4dd0307aee49d7b18579d15422e642
Eduardo Lima <dlima78@hotmail.com>
2021-12-05 08:45:47 -0300

    test: ensure DbSaveSurveyResult calls SaveSurveyResultRepository with correct values

---
+++ tests/data/usecases/db-save-survey-result.spec.ts
@@ -0,0 +1,53 @@
+import { SaveSurveyResultRepository } from '@/data/protocols'
+import { DbSaveSurveyResult } from '@/data/usecases'
+import { SaveSurveyResultModel } from '@/domain/usecases'
+import { SurveyResultModel } from '@/domain/models'
+import MockDate from 'mockdate'
+
+const mockSurveyResult = (): SurveyResultModel => ({
+  id: 'any_id',
+  surveyId: 'any_surveyId',
+  accountId: 'any_accountId',
+  answer: 'any_answer',
+  date: new Date()
+})
+
+const makeSaveSurveyResultRepository = (): SaveSurveyResultRepository => {
+  class SaveSurveyRepositorySpy implements SaveSurveyResultRepository {
+    async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+      return await Promise.resolve(mockSurveyResult())
+    }
+  }
+  return new SaveSurveyRepositorySpy()
+}
+
+type SutTypes = {
+  sut: DbSaveSurveyResult
+  saveSurveyResultRepositorySpy: SaveSurveyResultRepository
+}
+
+const makeSut = (): SutTypes => {
+  const saveSurveyResultRepositorySpy = makeSaveSurveyResultRepository()
+  const sut = new DbSaveSurveyResult(saveSurveyResultRepositorySpy)
+  return {
+    sut,
+    saveSurveyResultRepositorySpy
+  }
+}
+
+describe('DbSaveSurveyResult', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  test('Should call SaveSurveyRepository with correct valeus', async () => {
+    const { sut, saveSurveyResultRepositorySpy } = makeSut()
+    const saveSpy = jest.spyOn(saveSurveyResultRepositorySpy, 'save')
+    await sut.save(mockSurveyResult())
+    expect(saveSpy).toHaveBeenCalledWith(mockSurveyResult())
+  })
+})
