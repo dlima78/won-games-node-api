c7157e3e5652c2ea819319ed21b15aa1bb917b63
Eduardo Lima <dlima78@hotmail.com>
2021-12-09 09:22:22 -0300

    feat: add swagger API

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Won Games Node API',
+    description: 'API feita em nodejs baseado no curso do Rodrigo Manguinho',
+    version: '1.0.0'
+  }
+}
