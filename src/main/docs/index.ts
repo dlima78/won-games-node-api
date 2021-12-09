6d436df5ed5ff3de155aa8425cf025e03006d118
Eduardo Lima <dlima78@hotmail.com>
2021-12-09 11:20:50 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,25 @@
+import { loginPath } from './paths/login-path'
+import { accountSchema } from './schemas/account-schema'
+import { loginParamsSchema } from './schemas/login-params-schema copy'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Won Games Node API',
     description: 'API feita em nodejs baseado no curso do Rodrigo Manguinho',
     version: '1.0.0'
+  },
+  servers: [{
+    url: '/api'
+  }],
+  tags: [{
+    name: 'Login'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 }
